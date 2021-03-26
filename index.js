01c62b2343e6a32ced2da27585419806a43ed27a
lekaraidigima <you@example.com>
2021-03-26 16:00:44 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+ let itemPrice = 50;
+ let amountInWallet = 200;
+ let numberOfFriends = 5;
+ let age = 25;
+ const firstName = "Luka"
+ const lastName = "Idigima"
+ const midInitial = "J"
+
+ let newAmount = amountInWallet - itemPrice;
+ let friendsPerYear = Math.floor(age / numberOfFriends)
+ const fullName = ${firstName} +  ${midInitial} + ${lastNme}
+ 
+ console.log(`my Item price is ${itemPrice}
+              my amount in wallet is ${amountInWallet}
+              my number of friends is ${numberOfFriends}
+              my age is ${age}
+              my full name is ${fullName}
+              i have made ${friendsPerYear} each year
+              `)
